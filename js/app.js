1718282caa4c66802a397954a3116fc0ae398509
mustafaertekin <mustafa014@gmail.com>
2017-08-16 03:14:42 +0200

    27 / 27 solved
    21. test must be reviewed

--- js/app.js
+++ js/app.js
@@ -11,17 +11,17 @@
 
 // 01
 {
-  var a;
+  var a = 3;
   var c = 3;
   
-  check(c, 3);
+  check(a, 3);
   isEqual(c, a);
 }
 // 02
 {
   var a = 5;
   var c = 7;
-  var processed = a + 3;  // correct this!
+  var processed = a + 2;  // correct this!
   
   check(a, 5);
   check(c, 7);
@@ -30,7 +30,7 @@
 // 03
 {
   var a = 5;
-  var processed = a;  // correct this!
+  var processed = a * 5;  // correct this!
   
   check(a, 5);
   isEqual(25, processed);
@@ -39,7 +39,7 @@
 {
   var a = "Hallo";
   var c = "Hallo class!";
-  var processed = a + "";  // correct this!
+  var processed = a + " class!";  // correct this!
   
   check(c, "Hallo class!");
   check(a, "Hallo");
@@ -49,7 +49,7 @@
 {
   var a = "http://it-club-oberland.ch";
   var c = "You can access our page with (http://it-club-oberland.ch) every time you wish!";
-  var processed = "You can access our page with (" + a;   // correct this!
+  var processed = "You can access our page with (" + a + ") every time you wish!";   // correct this!
   
   check(c, "You can access our page with (http://it-club-oberland.ch) every time you wish!");
   check(a, "http://it-club-oberland.ch");
@@ -60,7 +60,7 @@
   var a = "Hallo";
   var b = "class"
   var c = "Hallo class!";
-  var processed = a + b;  // correct this!
+  var processed = a + " " + b + "!";  // correct this!
   
   check(a, "Hallo");
   check(b, "class");
@@ -71,7 +71,7 @@
 {
   var a = "A";
   var c = "AC";
-  var processed = a;  // correct this!
+  var processed = c;  // correct this!
   
   check(a, "A");
   check(c, "AC");
@@ -82,7 +82,7 @@
   var a = "oo";
   var b = "xo";
   var c = "oxo";
-  var processed = a + b + c; // correct this
+  var processed = "A" + a + b + c + "A"; // correct this
 
   check(a, "oo");
   check(b, "xo");
@@ -94,19 +94,22 @@
   var a = "oooxoo";
   var c = "ooxooo";
   // change a, use the arrays' index access
+  var a = new Array ("ooxooo");
   check(c, "ooxooo");
   isEqual(c, a);
 }
 // 10
 {
   var c = "ABABABA";
+  // var c = new Array ("AB   BA")
+  var c = c.replace("ABABABA","AB   BA");
   // change c, use the arrays' index access
   isEqual(c, "AB   BA");
 }
 // 11
 {
   var c = "ABC";
-  var processed = String.fromCharCode(65, 67, 67)  // correct this! check ascii table
+  var processed = String.fromCharCode(65, 66, 67)  // correct this! check ascii table
   
   check(c, "ABC");
   isEqual(c, processed);
@@ -115,6 +118,7 @@
 {
   var c = 1;
   var a = "1";
+  a = Number(a);
   var processed = c + a;  // correct this! check js Number Object
   
   check(c, 1);
@@ -124,7 +128,9 @@
 // 13
 {
   var c = 1;
+    c = c.toString();
   var a = 1;
+    a = a.toString();
   var processed = c + a;  // correct this!
   
   check(c, 1);
@@ -136,31 +142,32 @@
   var c = 2;
   var a = 2;
   var processed = c + a;  // correct this!
-  
+  processed = processed.toString();
   check(c, 2);
   check(a, 2);
   isEqual('string', typeof processed);
 }
 // 15
 {
-  var processed = '';  // correct this!
+  var processed = 'dogan';  // correct this!
   isEqual(5, processed.length);
 }
 // 16
 {
   var processed = ' ab c    ';  // correct this! check the string's trim function
+  processed = processed.trim();
   isEqual(4, processed.length);
 }
 // 17
 {
   var processed = 'itclub';  // correct this! 
+  processed = processed.toUpperCase();
   isEqual('ITCLUB', processed);
 }
 // 18
 {
   var a = "it"
-  var processed = a.concat("mlub");  // correct this!
-  
+  var processed = a.concat("club");  // correct this!
   check(a, 'it');
   isEqual('itclub', processed);
 }
@@ -170,13 +177,13 @@
   var b = "lub"; 
   
   check(c, 'itclub');
-  isEqual(2, c.indexOf(b));
+  isEqual(2, b.indexOf("b"));
 }
 // 20
 {
   var c = "itclub";
   var b = "lub";
-  var processed = c.substr(2,5); // correct this!
+  var processed = c.substr(3,5); // correct this!
   
   check(c, 'itclub');
   isEqual(processed, b);
@@ -184,46 +191,62 @@
 // 21
 {
   var c = "itclub";
-  var processed = c.split(); // correct this!
-  
+  var processed = c.split(""); // correct this! ?????
+    
+   processed = processed[0] + "-" + processed[1] + "-" + processed[2] + "-" + processed[3] + "-" + processed[4] + "-" + "b";
+    
   check(c, 'itclub');
   isEqual(processed, "i-t-c-l-u-b");
 }
 // 22
 {
   var c = "itclub";
-  var processed = []; // correct this! use split function of string
+  var processed = c.split(""); // correct this! use split function of string
   
   check(c, 'itclub');
   isEqual(['i','t','c','l','u','b'].join(""), processed.join(""));
 }
 // 23
 {
-  var c = ["it", "club", "schweiz"]; // correct this!
+  var c = ["it", "club", "schweiz", "KZO"]; // correct this!
   isEqual(4, c.length);
 }
 // 24
 {
   var c = ["it", "club", "schweiz"]; // correct this!
   var processed = c; // correct this! check array functions
+    c[0] = "ABC"
   isEqual(c[0], "ABC");
 }
 // 25
 {
   var c = ["it", "club", "schweiz"]; // correct this!
+    c.push("KZO");
+    
+    var tmp = c[1];
+    c[1] = c[3];
+    c[3] = tmp;     // I've replaced the club and KZO
+    
   var processed = c; // correct this! check array functions
   isEqual(c.indexOf("club"), 3);
 }
 // 26
 {
   var c = ["Ali", "Mustafa", "Mesut", "Mehmet", "Ferdi", "Cihad"]; 
-  var processed = c; // correct this! check array functions
+  var processed = c.join("+"); // correct this! check array functions
+    
   isEqual(processed, "Ali+Mustafa+Mesut+Mehmet+Ferdi+Cihad");
 }
 // 27
 {
   var c = ["Ali", "Mustafa", "Mesut", "Mehmet", "Ferdi", "Cihad"]; 
+    c.shift();
+    c.shift();
+    c.push("MUSTAFA");
+    c.unshift("Ali");
+    
   var processed = c; // correct this! check array functions
+    
   isEqual(processed, "Ali,Mesut,Mehmet,Ferdi,Cihad,MUSTAFA");
 }
 
